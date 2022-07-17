9ac870a7acea5e3c5417d11107297df6fef4533a
tobymac208 <niklas_fernandez@comcast.net>
2022-07-17 12:16:05 -0500

    Refactors: executes only server-side code for running nmap.

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 	<head>
 		<title>Nmapper</title>
 		<style>
+			.console {
+				border: 5px red;
+			}
 			.foreground {
 				color: #FFFFFF;
 			}
-			
 			.header {
 				display: block;
 				text-align: center;
 			}
-			
 			.input {
 				margin: 10px 0px 10px 0;
 			}
@@ -22,20 +23,18 @@
 		</style>
 	</head>
 	<body>
-		<script>
-			var SubmitNmapRequest = function () {
-				ip = document.getElementById('id').innerHTML;
-			};
-		</script>
 		<div class="foreground">
 			<div class="header">
 				<h1>Nmapper</h1>
 				<p>An Nmap tool created with PHP and bash :)</p>
 			</div>
-			
-			<div class="input">
-				<input id="ip" type="text" />
-				<button onclick="SubmitNmapRequest()" type="submit">Submit</button>
+			<div class="console">
+			<?php
+				# calls nmap with loopback (localhost) address
+				$nmap_response = shell_exec("./scripts/nmap_runner.sh 127.0.0.1");
+				# print out the result of the nmap scan
+				echo "<p>Nmap scan: $nmap_response</p>";
+			?>
 			</div>
 		</div>
 	</body>
