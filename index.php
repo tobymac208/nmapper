ae88408ec3a02b2825d9c4ac88b1c74709f0be98
tobymac208 <niklas_fernandez@comcast.net>
2022-07-17 02:31:55 -0500

    Fixing stupid idea of having HTML tags in JS. Ugh, I'm tired.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Nmapper</title>
@@ -20,33 +20,24 @@
 				background-color: #000000;
 			}
 		</style>
-		<script type="text/javascript">
-			var submitNmapRequest = function(ipAddress) {
-				<?php
-					# attempt to nmap the desired ip address	
-					$result = shell_exec("./scripts/nmap_runner.sh " + ipAddress);
-					echo "Server response: $result";
-				?>	
-			}
-		</script>
 	</head>
 	<body>
+		<script>
+			var SubmitNmapRequest = function () {
+				ip = document.getElementById('id').innerHTML;
+			};
+		</script>
 		<div class="foreground">
-			<?php
-				echo "Executing nmap script: ";
-				$result = shell_exec("./scripts/nmap_runner.sh");
-				echo "$result"	
-			?>
 			<div class="header">
 				<h1>Nmapper</h1>
 				<p>An Nmap tool created with PHP and bash :)</p>
 			</div>
 			
 			<div class="input">
-				<input type="text" />
-				<button type="submit">Submit</button>
+				<input id="ip" type="text" />
+				<button onclick="SubmitNmapRequest()" type="submit">Submit</button>
 			</div>
 		</div>
-
 	</body>
 </html>
+
